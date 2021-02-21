cef411098f5f350e85e21042679f56240a8b13f8
Rae Stichter <raestichter@gmail.com>
2021-02-21 15:21:20 -0500

    completed product model

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,9 @@
 // import important parts of sequelize library
+const database = require('mime-db');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { truncate } = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,6 +11,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal:true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
     // define columns
   },
   {
